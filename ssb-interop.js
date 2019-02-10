69d6798ec3bb6e9c6c3b0da59ec3fe5de1d61a00
Michael W. Martin <mwm.cal@gmail.com>
2019-02-10 09:20:34 -0700

    Create ssb-interop.js

---
+++ ssb-interop.js
@@ -0,0 +1,56 @@
+
+// First make sure the wrapper app is loaded
+document.addEventListener("DOMContentLoaded", function() {
+
+  // Then get its webviews
+  let webviews = document.querySelectorAll(".TeamView webview");
+
+  // Fetch our CSS in parallel ahead of time
+  const cssPath = 'https://cdn.rawgit.com/widget-/slack-black-theme/master/custom.css';
+  let cssPromise = fetch(cssPath).then(response => response.text());
+
+  let customCustomCSS = `
+  :root {
+     /* Modify these to change your theme colors: */
+     primary: #09F !important;
+     text: #CCC !important;
+     background: #080808 !important;
+     background-elevated: #222 !important;
+  }
+  .c-scrollbar__hider {
+    background-color: #080808 !important;
+  }
+  .c-message--light .c-message__sender .c-message__sender_link {
+    color: #CCC !important;
+  }
+  .c-message__body {
+    color: #CCC !important;
+  }
+  `
+
+  // Insert a style tag into the wrapper view
+  cssPromise.then(css => {
+     let s = document.createElement('style');
+     s.type = 'text/css';
+     s.innerHTML = css + customCustomCSS;
+     document.head.appendChild(s);
+  });
+
+  // Wait for each webview to load
+  webviews.forEach(webview => {
+     webview.addEventListener('ipc-message', message => {
+        if (message.channel == 'didFinishLoading')
+           // Finally add the CSS into the webview
+           cssPromise.then(css => {
+              let script = `
+                    let s = document.createElement('style');
+                    s.type = 'text/css';
+                    s.id = 'slack-custom-css';
+                    s.innerHTML = \`${css + customCustomCSS}\`;
+                    document.head.appendChild(s);
+                    `
+              webview.executeJavaScript(script);
+           })
+     });
+  });
+});
